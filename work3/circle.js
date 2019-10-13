e1302207ceea7c6bf5e6d881d675d9db1e7e458b
zhuangshanshang <guozishanshan@qq.com>
2019-10-13 16:24:13 +0800

    22

--- work3/circle.js
+++ work3/circle.js
@@ -3,7 +3,7 @@ r = parseFloat(r)&&Number(r)
 if (!isNaN(r)){
     var cir=2*Math.PI*r
     var area=Math.PI*r*r
-    document.getElementById('r').value
+    document.getElementById('r').value=r
     document.getElementById('cir').value=cir.toFixed(2)
     document.getElementById('area').value=area.toFixed(2)
 }else{
