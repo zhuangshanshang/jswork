89f12c7dffd06c7ab78bd68f7eca588350a21aec
zhuangshanshang <guozishanshan@qq.com>
2019-12-01 13:21:04 +0800

    work12

--- work10/ss.js
+++ work10/ss.js
@@ -1,4 +1,8 @@
 function stat(){
     let str = document.getElementById("str").value
     let obj = {}
+    var n = str.split(" ");
+    array.reduce(array)
+    return 
+    document .getElementById('result').innerText =JSON.stringify(obj)
 }
---
+++ work12/index.js
@@ -0,0 +1,8 @@
+let year =new Date().getFullYear()//获取当前年份
+document.getElementById("cal").innerHTML = calendar(year)
+
+function setCalendar(year){
+    
+    
+
+}
