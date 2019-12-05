1e55eba60b0a22bea51dcae842be4f784b244992
zhuangshanshang <guozishanshan@qq.com>
2019-12-05 20:51:54 +0800

    work14

--- work13/FormBuilder.js
+++ work13/FormBuilder.js
@@ -2,26 +2,26 @@
     var FormBuilder = function(data){
         this.data =data;
     };
-    FormBuilder.prototype.creat =function(){
+    FormBuilder.prototype.create =function(){
         var html ='';
         for(var k in this.data){
             var item ={tag:'',text:'',attr: {},option:null};
             for(var n in this.data[k]){
                 item[n] =this.data[k][n];
             }
-            html +=FormBuilder.toHTML(item);
+            html += builder.toHTML(item);
         }
-        return '<table>'+html+'<table>';
+        return '<table>'+html+'</table>';
     };
     var builder ={
         toHTML:function(obj){
             var html =this.item[obj.tag](this.attr(obj.attr),obj.option);
-            return '<tr><th>'+obj.text+'</tr><td>'+html+'</td></tr>';
+            return '<tr><th>'+obj.text+'</th><td>'+html+'</td></tr>';
         },
         attr:function(attr){
             var html='';
             for(var k in attr){
-                html += k+'='+attr[k]+'" ';
+                html += k+'="'+attr[k]+'" ';
             }
             return html;
         },
@@ -29,7 +29,7 @@
             input:function(attr,option){
                 var html ='';
                 if(option === null){
-                    html +='<input '+ attr +'>';
+                    html +='<input '+ attr + '>';
                 }else{
                     for(var k in option){
                         html +='<label><input '+ attr +' value="'+k+'"'+'>'+option[k]+'</label>' ;
@@ -37,15 +37,15 @@
                  }
                  return html;
                 },
-                select:function(attr,option){
+                select: function(attr,option){
                     var html ='';
                     for(var k in option){
-                        html +='<option value="' +k+'">' +option[k]+'<option>';
+                        html +='<option value="' +k+'">' +option[k]+'</option>';
                     }
-                    return '<select' +attr+'>'+html+'<select>';
+                    return '<select ' +attr+'>'+html+'</select>';
                 },
-                textarea:function(attr){
-                    return '<textarea' +attr+'>'+html+'</select>';
+                textarea: function(attr){
+                    return '<textarea ' +attr+'></textarea>';
                 }
         }
     };
